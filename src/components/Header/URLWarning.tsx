9d35704cb8b024056bc1c1fd343e1427d449630d
notEternal <cent1pede@protonmail.ch>
2021-01-23 20:06:07 +0800

    Replace domain name

--- src/components/Header/URLWarning.tsx
+++ src/components/Header/URLWarning.tsx
@@ -30,16 +30,16 @@ export default function URLWarning() {
     <PhishAlert isActive={showURLWarning}>
       <div style={{ display: 'flex' }}>
         <AlertTriangle style={{ marginRight: 6 }} size={12} /> Make sure the URL is
-        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>snakeswap.org</code>
+        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>app.snakeswap.org</code>
       </div>
       <StyledClose size={12} onClick={toggleURLWarning} />
     </PhishAlert>
-  ) : window.location.hostname === 'snakeswap.org' ? (
+  ) : window.location.hostname === 'app.snakeswap.org' ? (
     <PhishAlert isActive={showURLWarning}>
       <div style={{ display: 'flex' }}>
         <AlertTriangle style={{ marginRight: 6 }} size={12} /> Always make sure the URL is
-        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>snakeswap.org</code> - bookmark it to
-        be safe.
+        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>app.snakeswap.org</code> - bookmark it
+        to be safe.
       </div>
       <StyledClose size={12} onClick={toggleURLWarning} />
     </PhishAlert>
--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,10 +1,10 @@
 // CORS localhost problem (this url doesn't work)
-export const DEFAULT_TOKEN_LIST_URL = 'https://snakeswap.org/token-list/tokenlist.json'
+export const DEFAULT_TOKEN_LIST_URL = 'https://app.snakeswap.org/token-list/tokenlist.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
   'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json',
   'https://umaproject.org/uma.tokenlist.json',
   'https://app.tryroll.com/tokens.json',
-  'https://tokens.coingecko.com/uniswap/all.json',
+  'https://tokens.coingecko.com/uniswap/all.json'
 ]
